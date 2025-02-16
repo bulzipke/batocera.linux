d2c84fd2a7731d632381cad66f7f32c8721ea557
Ali Beyaz <symbuzzer@users.noreply.github.com>
2025-02-16 12:15:12 +0300

    Update es-theme-knulli.mk

--- package/knulli/themes/es-theme-knulli/es-theme-knulli.mk
+++ package/knulli/themes/es-theme-knulli/es-theme-knulli.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Knulli"
 #
 ################################################################################
-# Version: Commits on February 15, 2025
-ES_THEME_KNULLI_VERSION = 1a26fe86399e4f418a539b0c3b720dda65a24506
+# Version: Commits on February 16, 2025
+ES_THEME_KNULLI_VERSION = ffe723fd493c0adece99b109fd134485c8c5daaf
 ES_THEME_KNULLI_SITE = $(call github,symbuzzer,es-theme-knulli,$(ES_THEME_KNULLI_VERSION))
 
 define ES_THEME_KNULLI_INSTALL_TARGET_CMDS
