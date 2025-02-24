f70eef4177cfe82bcce1b592ee70d83f31e5e3f3
Ali Beyaz <symbuzzer@users.noreply.github.com>
2025-02-24 09:30:29 +0300

    Update es-theme-knulli.mk

--- package/knulli/themes/es-theme-knulli/es-theme-knulli.mk
+++ package/knulli/themes/es-theme-knulli/es-theme-knulli.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on February 24, 2025
-ES_THEME_KNULLI_VERSION = bd8025bec26a5252f8ce92a5d3bf6f4728c5973a
+ES_THEME_KNULLI_VERSION = f0d0fb3d41da9de56c7751b9d47321f2c5e74335
 ES_THEME_KNULLI_SITE = $(call github,symbuzzer,es-theme-knulli,$(ES_THEME_KNULLI_VERSION))
 
 define ES_THEME_KNULLI_INSTALL_TARGET_CMDS
